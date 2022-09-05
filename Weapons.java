667df473e05a40ffcf8e6e12fb4ae5aed1633eb5
Fortune Raphael C. Bermudez <111546354+bermudezfc@users.noreply.github.com>
2022-09-05 22:29:26 +0800

    Create Weapons.java

---
+++ Weapons.java
@@ -0,0 +1,24 @@
+public class Weapon {
+
+    // Attributes
+    String name;
+    int damage;
+    String rarity;
+    float weight;
+    String description;
+
+    // Methods
+    public void showAllRifleAttributes() {
+    System.out.println("Name: " + name);
+    System.out.println("Damage: " + damage);
+    System.out.println("Rarity: " + rarity);
+    System.out.println("Weight: " + weight);
+    System.out.println("Description: " + description);
+    }
+
+
+    
+
+
+
+}
